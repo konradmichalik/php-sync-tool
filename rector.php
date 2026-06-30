0c6085a1ed2d80bce1af0940cf25d511b35e64d6
Konrad Michalik <km@move-elevator.de>
2026-06-30 09:55:29 +0200

    chore: add project scaffolding and tooling configuration

---
+++ rector.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the "php-sync-tool" Composer package.
+ *
+ * (c) 2026 Konrad Michalik <km@move-elevator.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+use Rector\Config\RectorConfig;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\TypeDeclaration\Rector\ClassMethod\AddVoidReturnTypeWhereNoReturnRector;
+use Rector\ValueObject\PhpVersion;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__.'/src',
+        __DIR__.'/tests',
+    ])
+    ->withPhpVersion(PhpVersion::PHP_82)
+    ->withSets([
+        LevelSetList::UP_TO_PHP_82,
+    ])
+    ->withComposerBased(symfony: true)
+    ->withRules([
+        AddVoidReturnTypeWhereNoReturnRector::class,
+    ])
+;
