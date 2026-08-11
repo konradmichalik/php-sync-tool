248409fc5c2310e9cd5515da0b8fde290a603bc9
Konrad Michalik <km@move-elevator.de>
2026-08-11 12:20:55 +0200

    feat: add TransferStrategyResolver

---
+++ src/Remote/Transfer/TransferStrategyResolver.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the "php-sync-tool" Composer package.
+ *
+ * (c) 2026 Konrad Michalik <km@move-elevator.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace KonradMichalik\SyncTool\Remote\Transfer;
+
+use KonradMichalik\SyncTool\Config\SyncConfig;
+use KonradMichalik\SyncTool\Enum\SyncMode;
+use KonradMichalik\SyncTool\Remote\{RsyncCommandBuilder, RunnerFactory, SshClientFactory};
+
+/**
+ * TransferStrategyResolver.
+ *
+ * @author Konrad Michalik <km@move-elevator.de>
+ * @license GPL-3.0-or-later
+ */
+final readonly class TransferStrategyResolver
+{
+    public function __construct(
+        private RunnerFactory $runners = new RunnerFactory(),
+        private RsyncCommandBuilder $rsync = new RsyncCommandBuilder(),
+        private SshClientFactory $sshClientFactory = new SshClientFactory(),
+    ) {}
+
+    public function resolve(SyncConfig $config, SyncMode $mode): TransferStrategy
+    {
+        $originRemote = $config->origin->isRemote();
+        $targetRemote = $config->target->isRemote();
+
+        if (!$originRemote && !$targetRemote) {
+            return new RsyncTransferStrategy($this->runners, $this->rsync);
+        }
+
+        if (!$config->useRsync) {
+            return new SftpTransferStrategy($this->sshClientFactory);
+        }
+
+        if (SyncMode::Proxy === $mode) {
+            return new ProxyTransferStrategy($this->runners, $this->rsync);
+        }
+
+        if (SyncMode::SyncRemote === $mode) {
+            return new RemoteCopyTransferStrategy($this->runners, $this->rsync);
+        }
+
+        return new RsyncTransferStrategy($this->runners, $this->rsync);
+    }
+}
---
+++ tests/Unit/Remote/Transfer/TransferStrategyResolverTest.php
@@ -0,0 +1,93 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the "php-sync-tool" Composer package.
+ *
+ * (c) 2026 Konrad Michalik <km@move-elevator.de>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace KonradMichalik\SyncTool\Tests\Unit\Remote\Transfer;
+
+use KonradMichalik\SyncTool\Config\SyncConfig;
+use KonradMichalik\SyncTool\Enum\SyncMode;
+use KonradMichalik\SyncTool\Remote\Transfer\{ProxyTransferStrategy, RemoteCopyTransferStrategy, RsyncTransferStrategy, SftpTransferStrategy, TransferStrategyResolver};
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * TransferStrategyResolverTest.
+ *
+ * @author Konrad Michalik <km@move-elevator.de>
+ * @license GPL-3.0-or-later
+ */
+final class TransferStrategyResolverTest extends TestCase
+{
+    private TransferStrategyResolver $resolver;
+
+    protected function setUp(): void
+    {
+        $this->resolver = new TransferStrategyResolver();
+    }
+
+    #[Test]
+    public function bothLocalAlwaysUsesRsyncRegardlessOfNoRsyncFlag(): void
+    {
+        $config = SyncConfig::fromArray([
+            'use_rsync' => false,
+            'origin' => ['path' => '/srv/a', 'db' => ['name' => 'a', 'user' => 'a', 'password' => 'a']],
+            'target' => ['path' => '/srv/b', 'db' => ['name' => 'b', 'user' => 'b', 'password' => 'b']],
+        ]);
+
+        self::assertInstanceOf(RsyncTransferStrategy::class, $this->resolver->resolve($config, SyncMode::SyncLocal));
+    }
+
+    #[Test]
+    public function noRsyncFlagSelectsSftpWhenExactlyOneSideIsRemote(): void
+    {
+        $config = SyncConfig::fromArray([
+            'use_rsync' => false,
+            'origin' => ['host' => 'o.example.com', 'user' => 'deploy', 'db' => ['name' => 'a', 'user' => 'a', 'password' => 'a']],
+            'target' => ['path' => '/srv/b', 'db' => ['name' => 'b', 'user' => 'b', 'password' => 'b']],
+        ]);
+
+        self::assertInstanceOf(SftpTransferStrategy::class, $this->resolver->resolve($config, SyncMode::Receiver));
+    }
+
+    #[Test]
+    public function proxyModeSelectsProxyStrategy(): void
+    {
+        $config = SyncConfig::fromArray([
+            'origin' => ['host' => 'o.example.com', 'user' => 'deploy', 'db' => ['name' => 'a', 'user' => 'a', 'password' => 'a']],
+            'target' => ['host' => 't.example.com', 'user' => 'deploy', 'db' => ['name' => 'b', 'user' => 'b', 'password' => 'b']],
+        ]);
+
+        self::assertInstanceOf(ProxyTransferStrategy::class, $this->resolver->resolve($config, SyncMode::Proxy));
+    }
+
+    #[Test]
+    public function syncRemoteModeSelectsRemoteCopyStrategy(): void
+    {
+        $config = SyncConfig::fromArray([
+            'origin' => ['host' => 'o.example.com', 'user' => 'deploy', 'db' => ['name' => 'a', 'user' => 'a', 'password' => 'a']],
+            'target' => ['host' => 't.example.com', 'user' => 'deploy', 'db' => ['name' => 'b', 'user' => 'b', 'password' => 'b']],
+        ]);
+
+        self::assertInstanceOf(RemoteCopyTransferStrategy::class, $this->resolver->resolve($config, SyncMode::SyncRemote));
+    }
+
+    #[Test]
+    public function receiverModeDefaultsToRsyncStrategy(): void
+    {
+        $config = SyncConfig::fromArray([
+            'origin' => ['host' => 'o.example.com', 'user' => 'deploy', 'db' => ['name' => 'a', 'user' => 'a', 'password' => 'a']],
+            'target' => ['path' => '/srv/b', 'db' => ['name' => 'b', 'user' => 'b', 'password' => 'b']],
+        ]);
+
+        self::assertInstanceOf(RsyncTransferStrategy::class, $this->resolver->resolve($config, SyncMode::Receiver));
+    }
+}
